8ff985101de4d08358dc4412dc925fc4ca2dd03c
Giacomo F.M. <giacomofm@outlook.it>
2025-07-05 22:07:55 +0200

    Fix op8

--- users.nix
+++ users.nix
@@ -10,11 +10,11 @@
     packages = with pkgs; [
       jetbrains-toolbox
     ];
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    };
+  };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 }
